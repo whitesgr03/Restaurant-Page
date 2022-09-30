ccc7b4ebb49c512ab6c45c3d32b997d78c1cca7e
Weiss Bai <whitesgr03@gmail.com>
2022-09-30 17:36:36 +0800

    Add index.js to render home page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('hello world');
+'use strict'
+
+import {createHomePage as home } from './js/home'
+import './css/style.css';
+
+const content = document.querySelector('.content')
+
+content.innerHTML = home();
