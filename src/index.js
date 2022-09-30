157dfb47e5300d121899ac2d912e82710b3f6d34
Weiss Bai <whitesgr03@gmail.com>
2022-09-30 17:40:46 +0800

    Add switchPage function

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 'use strict'
 
 import {createHomePage as home } from './js/home'
+import {createMenuPage as menu }  from './js/menu'
 import './css/style.css';
 
+const container = document.querySelector('.container')
 const content = document.querySelector('.content')
 
+container.addEventListener('click', switchPage)
+
 content.innerHTML = home();
+
+function switchPage(e) {
+    if (e.target.tagName !== 'BUTTON') return
+
+    const page = e.target.className;
+    const handleRender = {
+        home,
+        menu,
+    }
+
+    const template = handleRender[page]()
+    content.innerHTML = template
+}
